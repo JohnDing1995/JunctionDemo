448fc54727c0280af016946f7daf0be91dc1c7b1
dry950526@gmail.com <dry950526@gmail.com>
2018-11-24 20:59:29 +0200

    transaction and user

--- backend/Junction/app.js
+++ backend/Junction/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser')
 var transferRouter = require('./routes/transfer');
 var userRouter = require('./routes/user');
 var buyRouter = require('./routes/buy');
@@ -14,11 +14,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/transfer', transferRouter);
 app.use('/user', userRouter);
