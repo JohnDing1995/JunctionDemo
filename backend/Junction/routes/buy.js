2084bbdcb70501b6b5d748336bfd5d3231167987
dry950526@gmail.com <dry950526@gmail.com>
2018-11-24 21:34:47 +0200

    ready to go

--- backend/Junction/routes/buy.js
+++ backend/Junction/routes/buy.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 var mongoose  = require('mongoose');
 const DB_URL = 'mongodb://localhost:27017/junction'
-
+var Transfer = require('../models/transferModel')
+var User = require('../models/userModel')
 var db = mongoose.connect(DB_URL)
 
 
-/* add a transfer. */
+/* buy something. */
 router.post('/', function(req, res, next) {
 
-    var name = req.quary.name
+    var name = req.query.name
     var amount = req.query.amount
 
-    User.findOne({'name':from}, function(err, payer){
+    User.findOne({'name':name}, function(err, payer){
       console.log(payer.balance)
       if(err) res.status(500).send(err)
       if(payer.balance - amount < 0){
@@ -20,18 +21,19 @@ router.post('/', function(req, res, next) {
       }
       payer.balance = parseFloat(payer.balance)-parseFloat(amount)
       payer.save(function(err){ if(err) res.status(500).send({'message': err})})
-
+      var newTrans = Transfer({from:name, to:"merchant", amount: amount})
+      newTrans.save(function(err){
+          if(err) res.status(500)
+      })
       res.json({
-        message: 'Paid'
-        name: payer.name
+        message: 'Paid',
+        name: payer.name,
         balance: payer.balance
       })
     });
 })
 
-/* buy something. */
-router.post('/', function(req, res, next) {
 
-});
+
 
 module.exports = router;
