5dc640ec01d4ebfc42f658caa679b8ead5d1eb2e
David Mikulovsky <david.2.mikulovsky@aalto.fi>
2018-11-24 21:23:41 +0200

    buy method

--- backend/Junction/routes/buy.js
+++ backend/Junction/routes/buy.js
@@ -5,9 +5,33 @@ const DB_URL = 'mongodb://localhost:27017/junction'
 
 var db = mongoose.connect(DB_URL)
 
+
+/* add a transfer. */
+router.post('/', function(req, res, next) {
+
+    var name = req.quary.name
+    var amount = req.query.amount
+
+    User.findOne({'name':from}, function(err, payer){
+      console.log(payer.balance)
+      if(err) res.status(500).send(err)
+      if(payer.balance - amount < 0){
+          res.status(400)
+      }
+      payer.balance = parseFloat(payer.balance)-parseFloat(amount)
+      payer.save(function(err){ if(err) res.status(500).send({'message': err})})
+
+      res.json({
+        message: 'Paid'
+        name: payer.name
+        balance: payer.balance
+      })
+    });
+})
+
 /* buy something. */
 router.post('/', function(req, res, next) {
-  
+
 });
 
 module.exports = router;
