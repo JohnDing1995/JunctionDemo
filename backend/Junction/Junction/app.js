f259c8aadbbba712d2505c5f9c13a7346e2e393f
dry950526@gmail.com <dry950526@gmail.com>
2018-11-24 22:00:33 +0200

    complete

--- backend/Junction/Junction/app.js
+++ backend/Junction/Junction/app.js
@@ -3,34 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
-var transferRouter = require('./routes/transfer');
-var userRouter = require('./routes/user');
-var buyRouter = require('./routes/buy');
-var cors = require('cors')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded());
-
-app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
-app.use(cors())
-app.use('/transfer', transferRouter);
-app.use('/user', userRouter);
-app.use('/buy', buyRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
